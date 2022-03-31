d619f1dc96bc38f7094134a563baab45aa8900e0
douglaslb <doug.lb07@gmail.com>
2022-03-30 22:17:47 -0300

    Adding prettier config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const foo = 'bla'
-
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
