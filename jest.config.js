e76b646a99902c7df6000776f6138ee545ca866b
douglaslb <doug.lb07@gmail.com>
2022-04-02 21:40:52 -0300

    Adding styled-components and removing babelrc in order to use Next SWC compiler

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest()
+
+const customJestConfig = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
+
+module.exports = createJestConfig(customJestConfig)
